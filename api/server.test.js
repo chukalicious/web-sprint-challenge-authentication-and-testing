7ba1d1db43f795e93033e4d2256276f196fb2756
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-21 18:46:26 -0800

    added simple test and it runs

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,15 @@
 // Write your tests here
+
+const request = require("supertest");
+const server = require("./server.js");
+const db = require("../data/dbConfig");
+
 test("sanity", () => {
   expect(true).toBe(true);
+});
+
+describe("Post to /register tests", () => {
+  test("waits until I'm ready", () => {
+    expect(true).toBe(true);
+  });
 });
