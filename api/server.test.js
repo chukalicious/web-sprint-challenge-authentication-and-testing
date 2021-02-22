a0827c802d796934a62f84105ca3baaeb30ab5b2
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-21 19:25:54 -0800

    one of each tests running

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 // Write your tests here
 
 const request = require("supertest");
-const server = require("./server.js");
+const server = require("./server");
 const db = require("../data/dbConfig");
 
 test("sanity", () => {
@@ -9,7 +9,26 @@ test("sanity", () => {
 });
 
 describe("Post to /register tests", () => {
-  test("waits until I'm ready", () => {
-    expect(true).toBe(true);
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  test("sends username and password for a new user to the server", async () => {
+    await request(server)
+      .post("/api/auth/register")
+      .send({ password: "password", username: "kat" });
+    const [user] = await db("users");
+    expect(user.username).toBe("kat");
+  });
+});
+
+describe("Post to the /login tests", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  test("returns token after login", async () => {
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "katiuska", password: "password" });
+    //incomplete
   });
 });
